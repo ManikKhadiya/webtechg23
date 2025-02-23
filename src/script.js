1b170d69a855a45f8ac730e5e0533a938a31a7e5
Manik Khadiya <manikkhadiya@gmail.com>
2025-02-23 15:06:44 +0000

    Update script.js

--- src/script.js
+++ src/script.js
@@ -23,7 +23,7 @@ function startRandomQuiz() {
 }
 
 function loadNavbar() {
-    fetch("navbar.html")
+    fetch("components/navbar.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("navbar-container").innerHTML = data;
