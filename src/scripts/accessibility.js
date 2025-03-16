29e07f843507cc1d5ae22bfb1ecbb61240e0f959
ManikKhadiya <manikkhadiya@gmail.com>
2025-03-16 19:54:19 +0000

    new question types

--- src/scripts/accessibility.js
+++ src/scripts/accessibility.js
@@ -7,4 +7,20 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById('large-text-toggle').addEventListener('change', function() {
         document.body.style.fontSize = this.checked ? '1.5em' : '1em';
     });
+
+    document.addEventListener("keydown", function (event) {
+        let options = document.querySelectorAll(".quiz-option");
+        let index = Array.from(options).findIndex(option => option === document.activeElement);
+    
+        if (event.key === "ArrowDown") {
+            index = (index + 1) % options.length;
+            options[index].focus();
+        } else if (event.key === "ArrowUp") {
+            index = (index - 1 + options.length) % options.length;
+            options[index].focus();
+        } else if (event.key === "Enter") {
+            options[index].click();
+        }
+    });
+    
 });
