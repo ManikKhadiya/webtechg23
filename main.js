d2d530612b012dc315b0e513e2282a154b8c4de1
amymillar <40589664@live.napier.ac.uk>
2025-04-28 10:59:18 +0100

    Update navbar

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 // Global javascript to insert the navigation bar
 
-// Get the navbar HTML file
-fetch("navbar.html")
-// convert the response into text
-    .then(response => response.text())
-    .then(data => {
-        // Insert the navbar content into the container id
+// Find out what folder the page is in
+let currentPath = window.location.pathname; 
+let folder = currentPath.split("/").length - 2; // -2 because first "/" is empty
+
+// Create a path to go back to the main folder
+let goBack = "";
+for (let i = 0; i < folder; i++) {
+    goBack += "../";
+}
+
+// Get the nav bar html
+fetch(goBack + "navbar.html")
+    .then(function(response) {
+        return response.text();
+    })
+    .then(function(data) {
+        // Insert the navbar content into the container
         document.getElementById("navbar-container").innerHTML = data;
-    });
+});
